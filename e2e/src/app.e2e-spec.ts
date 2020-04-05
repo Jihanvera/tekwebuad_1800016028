30957d79f9551d9844504b77adcf6afa3b0a8f23
Jihanvera <jihanverapramayda3@gmail.com>
2020-04-05 09:05:18 +0700

    n

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('tekweb1800016028 app is running!');
+    expect(page.getTitleText()).toEqual('tekweb app is running!');
   });
 
   afterEach(async () => {
--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/tekweb1800016028'),
+      dir: require('path').join(__dirname, './coverage/tekweb'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
